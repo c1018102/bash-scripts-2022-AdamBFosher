72da759fc43cc49324ff1bf62b20e1499897deff
c1018102 <c1018102@hallam.shu.ac.uk>
2022-12-08 19:55:23 +0000

    First

--- bash.sh
+++ bash.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt upgrade -y
-'Hello'
+sudo apt install apache2 -y
+sudo systemctl status apache2
+sudo apt install mariadb-server mariadb-client -y
+sudo systemctl status mariadb
+sudo mysql_secure_installation
+sudo apt install php php-mysql php-xml php-mbstring libapache2-mod-php -y
+
