1929ed9e8cc62810ddaee5663c9dc7a37fae12eb
c1018102 <c1018102@hallam.shu.ac.uk>
2022-12-08 20:26:45 +0000

    test

--- bash.sh
+++ bash.sh
@@ -15,8 +15,9 @@ sudo systemctl status mariadb
 sudo mysql_secure_installation
 sudo apt install php php-mysql php-xml php-mbstring libapache2-mod-php -y
 
-if sudo systemctl apache2 = active
- echo "Apache2 is running."
-elif
- echo "Apache2 is not running."
+if sudo systemctl status apache2 = active
+then
+  STATEMENTS1
+else
+  STATEMENTS2
 fi
