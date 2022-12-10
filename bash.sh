557da7f9a321f74c50014ad121fa2d2f85ac067a
c1018102 <c1018102@hallam.shu.ac.uk>
2022-12-10 12:11:48 +0000

    push

--- bash.sh
+++ bash.sh
@@ -25,6 +25,8 @@ sudo systemctl status mariadb
 sudo mysql_secure_installation
 sudo apt install php php-mysql php-xml php-mbstring libapache2-mod-php -y
 
+#Application Check
+echo ""
 echo "Application Install Checker"
 echo "1 - Apache2"
 echo "2 - Nginx"
